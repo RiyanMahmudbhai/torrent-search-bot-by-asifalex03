e258188493a65410c691b9e2fe1ca3c7383ebf30
Riyan Mahmud <118922773+RiyanMahmudbhai@users.noreply.github.com>
2025-02-20 23:19:46 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# Telegram Bot API Token
+TELEGRAM_API_TOKEN = 'YOUR_BOT_API_TOKEN'
+
+# Torrent Search Plugins and API settings
+SEARCH_PLUGINS = [
+    "https://gist.githubusercontent.com/scadams/56635407b8dfb8f5f7ede6873922ac8b/raw/f654c10468a0b9945bec9bf31e216993c9b7a961/one337x.py",
+    "https://raw.githubusercontent.com/BurningMop/qBittorrent-Search-Plugins/refs/heads/main/mypornclub.py",
+    "https://raw.githubusercontent.com/Larsluph/qbittorrent-search-plugins/prt/nova3/engines/pornrips.py",
+    "https://raw.githubusercontent.com/nindogo/qbtSearchScripts/master/torrentgalaxy.py",
+    "https://raw.githubusercontent.com/BurningMop/qBittorrent-Search-Plugins/refs/heads/main/xxxclubto.py",
+    "https://scare.ca/dl/qBittorrent/thepiratebay.py"
+]
+
+SEARCH_API_LINK = "https://your-api-url.com"  # If using an external API
+SEARCH_LIMIT = 10  # Number of results per search
